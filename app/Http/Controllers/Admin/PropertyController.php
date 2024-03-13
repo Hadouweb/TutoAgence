5ed88212ad0caa224ed8ebe8bf49983d28931290
Nicolas Le Breton <nicolas.le.breton.0xff@gmail.com>
2024-03-13 18:56:07 +0100

    Form

--- app/Http/Controllers/Admin/PropertyController.php
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\PropertyFormRequest;
 use Illuminate\Http\Request;
 use App\Models\Property;
 
@@ -20,15 +21,29 @@ class PropertyController extends Controller
      */
     public function create()
     {
-        //
+        $property = new Property();
+        $property->fill([
+            'surface' => 40,
+            'rooms' => 3,
+            'bedrooms' => 1,
+            'floor' => 0,
+            'city' => 'Montpellier',
+            'postal_code' => '34000',
+            'sold' => false,
+        ]);
+
+        return view('admin.properties.form', [
+            'property' => $property
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PropertyFormRequest $request)
     {
-        //
+        $property = Property::create($request->validated());
+        return to_route('admin.property.index')->with('success', 'Le bien a bien été créé');
     }
 
     /**
