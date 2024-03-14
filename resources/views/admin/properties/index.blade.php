f2fb3980d13be903cfabebd9b35fdfa598d52f53
Nicolas Le Breton <nicolas.le.breton.0xff@gmail.com>
2024-03-14 15:13:10 +0100

    sync

--- resources/views/admin/properties/index.blade.php
+++ resources/views/admin/properties/index.blade.php
@@ -22,9 +22,18 @@
                 <tr>
                     <td>{{ $property->title }}</td>
                     <td>{{ $property->surface }}m²</td>
-                    <td>{{ number_format($property->price, thousands_separator, ' ') }}</td>
+                    <td>{{ number_format($property->price, 2, ',', ' ') }}</td>
                     <td>{{ $property->city }}</td>
-                    <td></td>
+                    <td>
+                        <div class="d-flex gap-2 w-100 justify-content-end">
+                            <a href="{{ route('admin.property.edit', $property)}}" class="btn btn-primary">Editer</a>
+                            <form action="{{ route('admin.property.destroy', $property)}}" method="post">
+                                @csrf
+                                @method("delete")
+                                <button class="btn btn-danger">Supprimer</button>
+                            </form>
+                        </div>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
